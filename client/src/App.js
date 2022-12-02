809464ab79217b6900e2f2f4aba19ebc84ff9423
redwanratu <redwanulratu@gmail.com>
2022-12-03 04:51:10 +0600

    night1

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,32 @@
-import { useState, useEffect } from "react";
-import "./App.css";
-import Slot from "./components/Slot";
+import { useState, useEffect } from 'react';
+import './App.css';
+import Slot from './components/Slot';
 
-function App() {  const [slotState, setSlotState] = useState();
+function App() {
+  const [slotState, setSlotState] = useState();
 
   const fetchData = () => {
     return fetch(`${window.location.href}api/v1/status`)
-          .then((res) => res.json())
-          .then((data) => setSlotState(data.data[0]))
-          .catch((err) => console.log(err)); 
-  }
+      .then((res) => res.json())
+      .then((data) => setSlotState(data.data[0]))
+      .catch((err) => console.log(err));
+  };
 
   useEffect(() => {
-    fetchData()
+    fetchData();
   });
   // console.log(`${window.location.href}api/v1/status`)
   return (
     <div className="App">
       <main className="flex flex-col items-center justify-center mt-20">
-        <h1 className="text-3xl bold select-none">Automated Car Parking System</h1>
+        <h1 className="text-3xl bold select-none">
+          Automated Car Parking System
+        </h1>
         {slotState && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-8">
-            <Slot id={slotState.slot_1} />
-            <Slot id={slotState.slot_2} />
-            <Slot id={slotState.slot_3} />
+            <Slot id={slotState.slot_1} no={1} />
+            <Slot id={slotState.slot_2} no={2} />
+            <Slot id={slotState.slot_3} no={3} />
           </div>
         )}
       </main>
--- client/src/components/Slot.jsx
+++ client/src/components/Slot.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-export default function Slot({ id }) {
+export default function Slot({ id, no }) {
   const [empty] = React.useState(id === 1 ? true : false);
   const slotColor = empty ? "bg-green-600" : "bg-red-500";
 
   return (
     <div className="flex flex-col items-center">
-      <h2 className="text-2xl text-gray-500 select-none">Slot {id}</h2>
+      <h2 className="text-2xl text-gray-500 select-none">Slot {no}</h2>
       <div
         className={`flex items-center justify-center py-2 px-4 w-60 h-60 rounded mt-2 cursor-pointer ${slotColor}`}
       >
