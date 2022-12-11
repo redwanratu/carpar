39931d64e8776eec99b8cbc4e4c18dd7a5b896ff
redwanratu <39013689+redwanratu@users.noreply.github.com>
2022-12-12 00:50:37 +0600

    take post body

--- controllers/parkingSlotController.js
+++ controllers/parkingSlotController.js
@@ -13,28 +13,40 @@ exports.getSlotStatus = catchAsync(async (req, res, next) => {
 });
 
 exports.slotStatusHistory = catchAsync(async (req, res, next) => {
+  console.log('[NEW REQUEST]  : POST request..');
+  query = req.query;
+  console.log('[PROCESSING]   : processing query data');
+  console.log(`[DATA]         :`);
+  console.log(query);
 
-  
-  query=req.query;
+  body = req.body;
+  console.log('[PROCESSING]   : processing body data');
+  console.log(`[DATA]         : ${req.body}`);
+  console.log(body);
 
   const newHistory = await History.create({
-            slot_1: req.query.slot_1,
-            slot_2: req.query.slot_2,
-            slot_3: req.query.slot_3,
-       });
+    slot_1: req.body.slot_1,
+    slot_2: req.body.slot_2,
+    slot_3: req.body.slot_3,
+  });
 
-  await Status.deleteOne({ slot_1: {$gte : 0} });
+  // const newHistory = await History.create({
+  //           slot_1: req.query.slot_1,
+  //           slot_2: req.query.slot_2,
+  //           slot_3: req.query.slot_3,
+  //      });
+
+  await Status.deleteOne({ slot_1: { $gte: 0 } });
 
   const newStatus = await Status.create({
-    slot_1: req.query.slot_1,
-    slot_2: req.query.slot_2,
-    slot_3: req.query.slot_3,
-
-});
-
+    slot_1: req.body.slot_1,
+    slot_2: req.body.slot_2,
+    slot_3: req.body.slot_3,
+  });
+  console.log("     #######[SUCCESS]#######");  
   res.status(200).json({
     status: 'success',
-    data: query,
+    data: body,
   });
 });
 
